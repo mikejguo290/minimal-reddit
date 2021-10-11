2bd3e11bafe29e13c21b8b7bd3703ebfd9c9a521
mikejguo290 <mikejguo290@gmail.com>
2021-10-11 20:21:30 +0100

    create and style header elements including icon, app name and searchBar.

--- src/app/App.js
+++ src/app/App.js
@@ -1,8 +1,27 @@
 import { Page } from '../pages/Page';
 
+import redditImage from '../images/reddit.jpeg' /* local imports */
+
 function App() {
   return (
-    <Page/>
+    <>
+      <header>
+        <div class="brand">
+          <figure class="logo">
+            <img src={redditImage} alt="reddit favicon"/>
+          </figure>
+          <p class="appName">Reddit Minimal</p>
+        </div>
+        <input class="searchBar" placeholder='Search'/>
+        <div class="empty">
+        </div>
+      </header>
+
+      <main>
+
+        
+      </main>
+    </>
   );
 }
 
