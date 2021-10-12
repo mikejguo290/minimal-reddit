19ffb8697aee74f05d5141899e68f35c72847931
mikejguo290 <mikejguo290@gmail.com>
2021-10-12 11:37:48 +0100

    create and style elements and components including Posts, Post, Subreddits and Comments.

--- src/app/App.js
+++ src/app/App.js
@@ -18,7 +18,64 @@ function App() {
       </header>
 
       <main>
+        <div className="feed">
+          <div className="posts">
+            {/* start of post */}
+            <div className="post">
+              <div className="postVotes">
+                <p>2k</p>
+              </div>
+              <div className="postContext">
+                <p>r/subreddit_name</p>
+                <h3>Title here</h3>
+                <p>Image or Intro Text</p>
+                <div className="postmetaData">
+                  <p>author</p>
+                  <p>posted time</p>
+                  <p>Comments</p>
+                </div>
+              </div>
+            </div>
+            {/* end of post */}
 
+            {/* start of post */}
+            <div className="post">
+              <div className="postVotes">
+                <p>2k</p>
+              </div>
+              <div className="postContext">
+                <p>r/subreddit_name</p>
+                <h3>Title here</h3>
+                <p>Image or Intro Text</p>
+                <div className="postmetaData">
+                  <p>Author</p>
+                  <p>Posted time</p>
+                  <p>Comments</p>
+                </div>
+              </div>
+            </div>
+            {/* end of post */}
+          </div>
+        </div>
+        <section className="subredditsContainer">
+          <div className="subreddits">
+            <h2>Subreddits</h2>
+            <div className="subreddit">
+              <figure>
+                <img src={redditImage} alt='subreddit icon' />
+              </figure>
+              <h3>Javascript</h3>
+            </div>
+
+            <div className="subreddit">
+              <figure>
+                <img src={redditImage} alt='subreddit icon' />
+              </figure>
+              <h3>React</h3>
+            </div>
+
+          </div>
+        </section>
         
       </main>
     </>
