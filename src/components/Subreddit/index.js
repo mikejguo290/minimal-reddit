9a9be142268dc4a7914d81337e1c421f1860e364
mikejguo290 <mikejguo290@gmail.com>
2021-10-14 00:01:16 +0100

    create static data for subreddits on Subreddits and pass data to Subreddit via props.

--- src/components/Subreddit/index.js
+++ src/components/Subreddit/index.js
@@ -1,12 +1,13 @@
 import redditImage from '../../images/reddit.jpeg'
 
-export function Subreddit(){
+export function Subreddit(props){
+    const { name } = props.data;
     return (
         <div className="subreddit">
             <figure>
                 <img src={redditImage} alt='subreddit icon' />
             </figure>
-            <h3>Javascript</h3>
+            <h3>{name}</h3>
         </div>
     )
 }
--- src/features/Subreddits/index.js
+++ src/features/Subreddits/index.js
@@ -1,10 +1,25 @@
 import { Subreddit } from '../../components/Subreddit';
 export function Subreddits(){
+    const sub1 = {
+        name:'javascript',
+    }
+    const sub2 = {
+        name:'webdev',
+    }
+    const sub3 = {
+        name:'reactjs',
+    }
+    const sub4={
+        name:'learnprogramming',
+    }
+    const sub5={
+        name:'Programminghumor'
+    }
+    const subreddits = [sub1, sub2, sub3, sub4, sub5];
     return (
         <div className="subreddits">
             <h2>Subreddits</h2>
-            <Subreddit />
-            <Subreddit />
+            {subreddits.map(subreddit => <Subreddit data={subreddit} />)}
         </div>       
     );
 }
