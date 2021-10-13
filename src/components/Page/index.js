3afc4283155beff2dc490486cb9b6f833c0b3d94
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 22:47:42 +0100

    create static data for Posts on Page.

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,13 +3,41 @@ import { Posts } from '../../features/Posts';
 import { Banner } from '../Banner';
 
 export function Page(props){
+    const post1 = {
+        votes: 2000,
+        subreddit:'javascript',
+        title:'more frameworks required',
+        introText:'The world cries out for more javascript frameworks during frameworks shortage.',
+        postedTime:'6 hours ago',
+        comments:[],
+    };
+
+    const post2 = {
+        votes: 3000,
+        subreddit:'javascript',
+        title:'Is PHP better?',
+        introText:'Should I learn PHP instead of Javascript?',
+        postedTime:'3 hours ago',
+        comments:[],
+    };
+
+    const post3 = {
+        votes: 30,
+        subreddit:'javascript',
+        title:'JQuery use finally peaked in 2021 ',
+        introText:'JQuery decline has begun. We owe JQuery a huge debt',
+        postedTime:'1 hours ago',
+        comments:[],
+    };
     
+    const posts = [post1, post2, post3 ]
+
     return (
         <>  
             {true && <Banner />}
             <main>       
                 <div className="feed">
-                    <Posts />
+                    <Posts posts={posts}/>
                 </div>
             </main>
             <aside>
