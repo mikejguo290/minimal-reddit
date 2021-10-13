0f6eaef6a3d9b7de959722eb40158ba86a42ab97
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 15:09:45 +0100

    create a banner object and conditionally display it in Page within subreddit route.

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,10 +1,16 @@
 import { Subreddits } from '../../features/Subreddits';
 import { Posts } from '../../features/Posts';
 
-export function Page () {
+export function Page(props){
+    const banner = (
+        <div className="banner" >
+            <h2>Subreddit Name</h2>
+        </div>
+    );
     return (
-        <>
-            <main>        
+        <>  
+            {false && banner}
+            <main>       
                 <div className="feed">
                     <Posts />
                 </div>
