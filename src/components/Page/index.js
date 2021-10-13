183ef2bb3f51a68d38865d7487548ce4e7bcd4e6
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 13:12:34 +0100

    replace html with Posts component.

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,71 +1,12 @@
-import redditImage from '../../images/reddit.jpeg' /* local imports */
 import { Subreddits } from '../../features/Subreddits';
-import { Comments } from '../../features/Comments';
+import { Posts } from '../../features/Posts';
+
 export function Page () {
     return (
         <>
             <main>        
                 <div className="feed">
-                    <div className="posts">
-                        {/* start of post */}
-                        <div className="post">
-                        <div className="postVotes">
-                            <p>2k</p>
-                        </div>
-                        <div className="postContext">
-                            <p className="subRedditLink">r/subreddit_name</p>
-                            <h3>Enter the Pics Halloween photo!</h3>
-                            <p>Image or Intro Text</p>
-                            <div className="postmetaData">
-                                <p>author</p>
-                                <p>posted time</p>
-                                <p>Comments</p>
-                            </div>
-                        </div>
-                        </div>
-                        {/* end of post */}
-
-                        {/* start of post */}
-                        <div className="post">
-                        <div className="postVotes">
-                            <p>2k</p>
-                        </div>
-                        <div className="postContext">
-                            <p>r/subreddit_name</p>
-                            <h3>Title</h3>
-                            <p>Image or Intro Text</p>
-                            <div className="postmetaData">
-                                <p>Author</p>
-                                <p>Posted time</p>
-                                <p>Comments</p>
-                            </div>
-                        </div>
-                        </div>
-                        {/* end of post */}
-
-                        {/* start of post */}
-                        <div className="post">
-                        <div className="postVotes">
-                            <p>2k</p>
-                        </div>
-                        <div className="postContext">
-                            <p>r/subreddit_name</p>
-                            <h3>Title</h3>
-                            <p>Image or Intro Text</p>
-                            <div className="postmetaData">
-                                <p>Author</p>
-                                <p>Posted time</p>
-                                <p>Comments</p>
-                            </div>
-                            {/* start of comments */}
-                            <Comments />
-                            {/* end of comments */}
-
-                        </div>
-                        </div>
-                        {/* end of post */}
-                    </div>
-
+                    <Posts />
                 </div>
             </main>
             <aside>
