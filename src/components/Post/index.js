0a46891e83d09f41d98c381e7b5614dcb7e8a766
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 23:02:00 +0100

    create static data for Posts on Posts (instead of Page).

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -1,18 +1,28 @@
 
 import { Comments } from '../../features/Comments';
-export function Post(){
+export function Post(props){
+    const {
+        votes,
+        subreddit,
+        title,
+        introText,
+        author,
+        postedTime,
+        comments,
+    } = props.data;
+
     return (
         <div className="post">
             <div className="postVotes">
-                <p>2k</p>
+                <p>{votes}</p>
             </div>
             <div className="postContext">
-                <p>r/subreddit_name</p>
-                <h3>Title</h3>
-                <p>Image or Intro Text</p>
+                <p>{subreddit}</p>
+                <h3>{title}</h3>
+                <p>{introText}</p>
                 <div className="postmetaData">
-                    <p>Author</p>
-                    <p>Posted time</p>
+                    <p>By {author}</p>
+                    <p>{postedTime}</p>
                     <p>Comments</p>
                 </div>
                 {/* start of comments */}
--- src/features/Posts/index.js
+++ src/features/Posts/index.js
@@ -1,10 +1,39 @@
-import { Post } from '../../components/Post';
+import { Post } from '../../components/Post'
 export function Posts (){
+    const post1 = {
+        votes: 2000,
+        subreddit:'r/javascript',
+        title:'more frameworks required',
+        introText:'The world cries out for more javascript frameworks during frameworks shortage.',
+        author:'k.dodds',
+        postedTime:'6 hours ago',
+        comments:[],
+    };
+
+    const post2 = {
+        votes: 3000,
+        subreddit:'r/javascript',
+        title:'Is PHP better?',
+        introText:'Should I learn PHP instead of Javascript?',
+        author:'d.abramov',
+        postedTime:'3 hours ago',
+        comments:[],
+    };
+
+    const post3 = {
+        votes: 3000,
+        subreddit:'r/javascript',
+        title:'JQuery use finally peaked in 2021 ',
+        introText:'JQuery decline has begun. We owe JQuery a huge debt',
+        author:'m.appleton',
+        postedTime:'1 hours ago',
+        comments:[],
+    };
+    
+    const posts = [post1, post2, post3 ];
     return (
         <div className="posts">
-            <Post />
-            <Post />
-            <Post />
+            {posts.map(post => <Post data={post} />)}
         </div>
     );
 }
