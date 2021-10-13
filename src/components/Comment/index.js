cb1cf8121399d602c11006b525cc7839ed92a048
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 12:20:12 +0100

    start on static version of Comment component.

---
+++ src/components/Comment/index.js
@@ -0,0 +1,13 @@
+export function Comment(){
+    return (
+        <article className="comment">
+            <div className="commentData">
+                <h4 className="commenter">Commenter</h4>
+                <p className="commentText">Anything goes here.</p>
+            </div>
+            <div>
+                <p>time</p>
+            </div>
+        </article>
+    )
+}
