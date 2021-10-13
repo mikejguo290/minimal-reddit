1634e897c43e154f9d9fff80660de9ca6d1d4f6c
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 12:22:36 +0100

    start on static version of Comments component.

---
+++ src/features/Comments/index.js
@@ -0,0 +1,10 @@
+import { Comment } from '../../components/Comment';
+export function Comments(){
+    return (
+        <div className="comments">
+            <Comment />
+            <Comment />
+            <Comment />
+        </div>
+    )
+}
