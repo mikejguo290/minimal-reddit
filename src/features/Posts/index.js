0d64087f44107309dd0e8e0fe7cbc707d0dc52ca
mikejguo290 <mikejguo290@gmail.com>
2021-10-13 13:09:44 +0100

    start on static version of Posts component.

---
+++ src/features/Posts/index.js
@@ -0,0 +1,10 @@
+import { Post } from '../../components/Post';
+export function Posts (){
+    return (
+        <div className="posts">
+            <Post />
+            <Post />
+            <Post />
+        </div>
+    );
+}
