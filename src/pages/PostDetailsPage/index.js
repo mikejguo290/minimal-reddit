6e92354bf842670561af0889e9027a2f478ea9ad
mikejguo290 <mikejguo290@gmail.com>
2021-10-15 16:35:46 +0100

    create PostDetailsPage to show the details of one reddit post.

---
+++ src/pages/PostDetailsPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Page } from '../../components/Page';
+
+export function PostDetailsPage(){
+    const pageType = "detail"
+    return (
+        <Page type={pageType} />
+    );
+}
