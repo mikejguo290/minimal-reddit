7b07cad247d00c6ad43b8f8b1a0b7df68f4f11df
mikejguo290 <mikejguo290@gmail.com>
2021-10-15 15:25:56 +0100

    wrap components in Router and set up Route for Homepage.

--- src/app/App.js
+++ src/app/App.js
@@ -1,13 +1,15 @@
-import { Page } from '../components/Page';
 import { Search } from '../features/Search';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { Homepage } from '../pages/Homepage';
+import { BrowserRouter as Router , Route} from 'react-router-dom';
 
 function App() {
   return (
     <>
       <Search />
       <Router>
-        <Page /> {/* going to be subsituted for Homepage and others in a Router switch */}
+        <Route path="/" exact>
+          <Homepage />
+        </Route>
       </Router>
     </>
   );
