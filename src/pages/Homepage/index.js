2b8f16d61ea7e5b8dfda2c46e9afa4a6ec1536cf
mikejguo290 <mikejguo290@gmail.com>
2021-10-15 15:22:13 +0100

    create Homepage component and render Page with prop type.

---
+++ src/pages/Homepage/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Page } from '../../components/Page';
+
+export function Homepage(){
+    const pageType = "home"
+    return (
+        <Page type={pageType} />
+    );
+}
+
