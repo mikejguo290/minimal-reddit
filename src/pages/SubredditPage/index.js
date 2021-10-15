c82cc69293af148245f5930340fa1c09eb67afed
mikejguo290 <mikejguo290@gmail.com>
2021-10-15 15:29:08 +0100

    create subreddit page.

---
+++ src/pages/SubredditPage/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Page } from '../../components/Page';
+
+export function SubredditPage(){
+    const pageType = "subreddit"
+    return (
+        <Page type={pageType} />
+    );
+}
