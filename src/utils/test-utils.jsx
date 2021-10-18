8c77d7773fe8003991ef4ffb4ec55de3ef0a5d5c
mikejguo290 <mikejguo290@gmail.com>
2021-10-18 22:52:01 +0100

    start on test-utils to produce a bespoke render that includes browser-router.

---
+++ src/utils/test-utils.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {render} from '@testing-library/react'
+import { BrowserRouter as Router } from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+
+const AllTheProviders = ({children})=>{
+    const history = createMemoryHistory();
+    return (
+        <Router history={history}>
+            {children}
+        </Router>
+    )
+}
+
+const customRender = (ui, options) => {
+    return render(ui, {wrapper: AllTheProviders, ...options})
+}
+
+
+
+// re-export everything
+export * from '@testing-library/react'
+
+// override render method
+export {customRender as render}
