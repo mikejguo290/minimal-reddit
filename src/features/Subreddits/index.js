45416003901f43f4031898b5765fc41cb837e8a5
mikejguo290 <mikejguo290@gmail.com>
2021-10-18 13:45:03 +0100

    wrap subreddits within ul element and style it. add li key={subreddit.id).

--- src/features/Subreddits/index.js
+++ src/features/Subreddits/index.js
@@ -2,33 +2,42 @@ import { Subreddit } from '../../components/Subreddit';
 import { NavLink } from 'react-router-dom';
 export function Subreddits(){
     const sub1 = {
-        name:'javascript',
+        name:'javascript', /* subreddit display name */
+        id:'1',
     }
     const sub2 = {
         name:'webdev',
+        id:'2', 
     }
     const sub3 = {
         name:'reactjs',
+        id:'3', 
     }
     const sub4={
         name:'learnprogramming',
+        id:'4',
     }
     const sub5={
-        name:'Programminghumor'
+        name:'Programminghumor',
+        id:5,
     }
     const subreddits = [sub1, sub2, sub3, sub4, sub5];
     return (
         <div className="subreddits">
             <h2>Subreddits</h2>
+            <ul className="redditsList">
             {
                 subreddits.map(subreddit => {
                     return (
-                        <NavLink to={`/r/${subreddit.name}`} className="subredditNav" activeClassName="selectedSubreddit" > 
-                            <Subreddit data={subreddit} />
-                        </NavLink>
+                        <li key={subreddit.id}>
+                            <NavLink to={`/r/${subreddit.name}`} className="subredditNav" activeClassName="selectedSubreddit" > 
+                                <Subreddit data={subreddit} />
+                            </NavLink>
+                        </li>
                     )
                 })
             }
+            </ul>
         </div>       
     );
 }
