d686e690bd68d8ada9b9440b508c09f9f9eb35c7
mikejguo290 <mikejguo290@gmail.com>
2021-10-18 17:22:55 +0100

    start on passing props from Comments to Comment component.

--- src/features/Comments/index.js
+++ src/features/Comments/index.js
@@ -1,5 +1,9 @@
 import { Comment } from '../../components/Comment';
 export function Comments(){
+    /* comments will have to make a call to fetch reddit comments associated with one particular post. */
+    const comments = [
+        {},{},{}
+    ];
     return (
         <div className="comments">
             <Comment />
