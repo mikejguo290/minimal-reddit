f181c38092b743111273ee7f09fb1797b1c9365d
mikejguo290 <mikejguo290@gmail.com>
2021-10-19 11:49:24 +0100

    unit test Search component.

--- src/features/Search/__tests__/Search.test.js
+++ src/features/Search/__tests__/Search.test.js
@@ -1,8 +1,20 @@
-import React from 'react';
-import { render, screen } from '../../../utils/test-utils';
 import { Search } from '../index';
+import React from 'react';
+import { render, screen  } from '../../../utils/test-utils';
+import '@testing-library/jest-dom' /* import jest-dom to access the Dom-specific matcher methods. */
 
-test('renders input with no problem', ()=>{
+test('renders', ()=>{
     render(<Search/>);
-    screen.debug();
+});
+
+test('renders with app name', ()=>{
+    render(<Search/>);
+    const appName = screen.getByText('RedditMinimal');
+    expect(appName).toBeInTheDocument();
+});
+
+test('renders with input', ()=>{
+    render(<Search/>);
+    const inputField = screen.getByPlaceholderText(/search/i);
+    expect(inputField).toBeInTheDocument();
 });
