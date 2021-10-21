d7bbb9aad9bb4d9429d7d6067344305acc8df645
mikejguo290 <mikejguo290@gmail.com>
2021-10-21 23:43:30 +0100

    SubredditPage to call getPosts whenever it renders.

--- src/pages/SubredditPage/index.js
+++ src/pages/SubredditPage/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Page } from '../../components/Page';
-import { useParams } from 'react-router-dom' ;
+import { useParams } from 'react-router-dom';
+import redditAPI from '../../utils/redditAPI';
+import { useEffect } from 'react'
 
 export function SubredditPage(){
     const pageType = "subreddit"
     const { subreddit } = useParams();
     const params = {subreddit: subreddit, postId:null};
+
+    useEffect(()=>{
+        const fetchPosts = async()=>{
+            const posts = await redditAPI.getPosts(subreddit);
+            console.log(posts);
+        }
+        fetchPosts();
+    },[subreddit]);
+
     return (
         <Page type={pageType} params={params}/>
     );
