d4cfa7ea1ccf4cdba2968823d2b774aab6c6f53c
mikejguo290 <mikejguo290@gmail.com>
2021-10-21 14:09:18 +0100

    test page components correctly with Route wrappers, and passing {route:'x'} to render.

--- src/pages/Homepage/__tests__/Homepage.test.js
+++ src/pages/Homepage/__tests__/Homepage.test.js
@@ -2,18 +2,35 @@ import { Homepage } from '../index';
 import React from 'react';
 import { render, screen  } from '../../../utils/test-utils';
 import '@testing-library/jest-dom';
+import { Route } from 'react-router-dom';
 
 it('renders', ()=>{
-    render(<Homepage />);
+    render(
+        <Route path="/" exact>
+            <Homepage />
+        </Route>,
+    {
+        route:"/"
+    }
+);
     //screen.debug();
 });
 
 it('renders without the banner component',()=>{
     // arrange
-    render(<Homepage />);
+    render(
+            <Route path="/" exact>
+                <Homepage />
+            </Route>,
+        {
+            route:"/"
+        }
+    );
     // act
     const bannerH2Headings = screen.queryAllByRole('heading',{level:2});
     // expect 
     // there should be just one h2 headings, which is used in the aside subreddits. the other one is used in the banner.
     expect(bannerH2Headings.length).toBe(1);
 });
+
+// additional tests to render it with x number of posts and subreddits.
--- src/pages/SubredditPage/__tests__/SubredditPage.test.js
+++ src/pages/SubredditPage/__tests__/SubredditPage.test.js
@@ -2,18 +2,36 @@ import { SubredditPage } from '../index';
 import React from 'react';
 import { render, screen  } from '../../../utils/test-utils';
 import '@testing-library/jest-dom';
+import { Route } from 'react-router-dom';
 
 it('renders', ()=>{
-    render(<SubredditPage />);
+    render(
+        <Route path="/r/:subreddit">
+            <SubredditPage />
+        </Route>,
+        {
+            route:'/r/brum',
+        }
+    );
     //screen.debug();
 });
 
 it('renders with the banner component',()=>{
     // arrange
-    render(<SubredditPage />);
+    render(
+        <Route path="/r/:subreddit">
+            <SubredditPage />
+        </Route>,
+        {
+            route:'/r/brum',
+        }
+    );
+
     // act
-    const bannerH2Headings = screen.queryAllByRole('heading',{level:2});
+    const bannerH2Heading = screen.getByRole('heading',{level:2, name:/brum/i});
     // expect 
     // there should be just two h2 headings, one is used in the aside subreddits. the other one is used in the banner.
-    expect(bannerH2Headings.length).toBe(2);
+    expect(bannerH2Heading).toBeInTheDocument();
 });
+
+// additional tests to render it with x number of posts and subreddits.
