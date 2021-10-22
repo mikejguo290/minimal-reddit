3672cd95fdb40f5fa754afd0c85add150252770f
mikejguo290 <mikejguo290@gmail.com>
2021-10-22 11:04:56 +0100

    Posts to get data about posts from selectors instead of mock data.

--- src/features/Posts/index.js
+++ src/features/Posts/index.js
@@ -1,43 +1,12 @@
 import React from 'react';
 import { Post } from '../../components/Post'
+import { useSelector } from 'react-redux';
+import { selectPosts } from './postsSlice';
+
 export function Posts (props){
-    const post1 = {
-        postId: '1',
-        permalink:'/r/reactjs/comments/1/',
-        subreddit:'reactjs',
-        subreddit_name_prefixed:'r/reactjs',
-        votes: 2000,
-        title:'more frameworks required',
-        introText:'The world cries out for more javascript frameworks during frameworks shortage.',
-        author:'k.dodds',
-        postedTime:'6 hours ago',
-    };
-
-    const post2 = {
-        postId: '2',
-        permalink:'/r/javascript/comments/2/',
-        subreddit:'javascript',
-        subreddit_name_prefixed:'r/javascript',
-        title:'Is PHP better?',
-        votes: 3000,
-        introText:'Should I learn PHP instead of Javascript?',
-        author:'d.abramov',
-        postedTime:'3 hours ago',
-    };
-
-    const post3 = {
-        postId: '3',
-        permalink:'/r/webdev/comments/3/',
-        subreddit:'webdev',
-        subreddit_name_prefixed:'r/webdev',
-        votes: 3000,
-        title:'JQuery use finally peaked in 2021 ',
-        introText:'JQuery decline has begun. We owe JQuery a huge debt',
-        author:'m.appleton',
-        postedTime:'1 hours ago',
-    };
     
-    const posts = [post1, post2, post3 ];
+    const posts = useSelector(selectPosts);
+   
     return (
         <div className="posts">
             <ul className="postsList">
