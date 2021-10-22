bf5db14f12dd6764bdeac3b26ff64122fbf81d22
mikejguo290 <mikejguo290@gmail.com>
2021-10-22 10:53:02 +0100

    dispatch thunk action creator fetchPostsBySubreddit() on rendering SubredditPage.

--- src/pages/SubredditPage/index.js
+++ src/pages/SubredditPage/index.js
@@ -1,20 +1,19 @@
 import React from 'react';
 import { Page } from '../../components/Page';
 import { useParams } from 'react-router-dom';
-import redditAPI from '../../utils/redditAPI';
 import { useEffect } from 'react'
+import { fetchPostsBySubreddit } from '../../features/Posts/postsSlice';
+import { useDispatch } from 'react-redux'
 
 export function SubredditPage(){
     const pageType = "subreddit"
     const { subreddit } = useParams();
     const params = {subreddit:subreddit, postId:null}
+    const dispatch = useDispatch();
+    
     useEffect(()=>{
-        const fetchPosts = async()=>{
-            const posts = await redditAPI.getPosts(subreddit);
-            console.log(posts);
-        }
-        fetchPosts();
-    },[subreddit]);
+        dispatch(fetchPostsBySubreddit(subreddit))
+    },[subreddit,dispatch]);
 
     return (
         <Page type={pageType} params={params} />
