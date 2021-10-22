46b0b395e67d0c762a782fc9d83535e6aaa0b61e
mikejguo290 <mikejguo290@gmail.com>
2021-10-22 21:36:32 +0100

    modify details Page to dispatch a thunk action creator to call the api on render.

--- src/pages/PostDetailsPage/index.js
+++ src/pages/PostDetailsPage/index.js
@@ -1,11 +1,21 @@
 import React from 'react';
 import { Page } from '../../components/Page';
 import { useParams } from 'react-router-dom';
+import redditAPI from '../../utils/redditAPI';
+import { fetchPostsBySubredditAndPostId } from '../../features/Posts/postsSlice';
+import { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 
 export function PostDetailsPage(){
     const pageType = "detail";
     const { subreddit, postId } = useParams();
     const params = {subreddit:subreddit, postId:postId};
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(fetchPostsBySubredditAndPostId({subreddit:subreddit, postId:postId}));
+    },[dispatch,subreddit,postId])
+
     return (
         <Page type={pageType} params={params} />
     );
