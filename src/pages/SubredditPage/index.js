2675664a0451d6248fe4492a546c6c18249f4ce0
mikejguo290 <mikejguo290@gmail.com>
2021-10-23 22:56:56 +0100

    SubredditPage to discriminately fetch data if subreddit doesn't match those in existing posts. pass props down to Posts.

--- src/pages/SubredditPage/index.js
+++ src/pages/SubredditPage/index.js
@@ -2,20 +2,27 @@ import React from 'react';
 import { Page } from '../../components/Page';
 import { useParams } from 'react-router-dom';
 import { useEffect } from 'react'
-import { fetchPostsBySubreddit } from '../../features/Posts/postsSlice';
-import { useDispatch } from 'react-redux'
+import { fetchPostsBySubreddit , selectPosts } from '../../features/Posts/postsSlice';
+import { useDispatch , useSelector  } from 'react-redux'
 
 export function SubredditPage(){
     const pageType = "subreddit"
     const { subreddit } = useParams();
     const params = {subreddit:subreddit, postId:null}
+    const posts = useSelector(selectPosts); // once posts is updated, this page would rerender and pass filteredPost as props to page. 
+    const filteredPosts = posts.filter(post => post.subreddit === subreddit); 
+
     const dispatch = useDispatch();
     
     useEffect(()=>{
-        dispatch(fetchPostsBySubreddit(subreddit))
-    },[subreddit,dispatch]);
+        if(filteredPosts.length<=1){
+            // if subreddit posts less than or equal to 1, call api again. this is vulnerable to misspellings causing endless api calls.
+            // or if subreddit has no posts or just one post. 
+            dispatch(fetchPostsBySubreddit(subreddit))
+        }
+    },[subreddit,filteredPosts, dispatch]);
 
     return (
-        <Page type={pageType} params={params} />
+        <Page type={pageType} params={params} posts={filteredPosts}/>
     );
 }
