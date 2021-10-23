fb2d53741e2c954aa845ab1b5d270b1e8b29de2a
mikejguo290 <mikejguo290@gmail.com>
2021-10-23 23:39:33 +0100

    start on Homepage's side effect for loading all posts after rendering.

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import { Page } from '../../components/Page';
+import { useEffect } from 'react'
+import { useDispatch, useSelector  } from 'react-redux';
+import { fetchPostsBySubreddit , selectPosts } from '../../features/Posts/postsSlice';
 
 export function Homepage(){
     const pageType = "home"
     const params = {subreddit:null, postId:null}
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        dispatch(fetchPostsBySubreddit('webdev'));
+    },[dispatch]);
+
+    const posts = useSelector(selectPosts);
+
     return (
-        <Page type={pageType} params={params} />
+        <Page type={pageType} params={params} posts={posts}/>
     );
 }
 
