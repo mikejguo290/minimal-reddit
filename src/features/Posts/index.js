35f16a60f7597dce7e2f4494c49bc71361a6ed22
mikejguo290 <mikejguo290@gmail.com>
2021-10-25 14:55:52 +0100

    sync searchTerm value to search feature in store. use it to filter posts displayed.

--- src/features/Posts/index.js
+++ src/features/Posts/index.js
@@ -1,14 +1,24 @@
 import React from 'react';
-import { Post } from '../../components/Post'
+import { Post } from '../../components/Post';
+import { useSelector } from 'react-redux';
+import { selectSearch } from '../Search/searchSlice';
 
 export function Posts (props){
     
     const { pageType, posts } = props;
-    
+    const search = useSelector(selectSearch);
+    console.log(search);
     return (
         <div className="posts">
             <ul className="postsList">
-                {posts.map(post => <li key={post.id}><Post data={post} pageType={pageType}/></li>)}
+                {posts.filter(post => {
+                    if(search){
+                        return post.title.toLowerCase().includes(search)
+                    }else{
+                        return post;
+                    }
+                })
+                .map(post => <li key={post.id}><Post data={post} pageType={pageType}/></li>)}
             </ul>
         </div>
     );
--- src/features/Search/index.js
+++ src/features/Search/index.js
@@ -2,21 +2,33 @@ import React from 'react';
 import redditImage from '../../images/reddit.jpeg'; /* local imports */
 import { Link } from 'react-router-dom';
 import { useState } from 'react';
+import { setSearchTerm , selectSearch} from './searchSlice';
+import { useDispatch, useSelector } from 'react-redux'
+
 export function Search (){
-    const [ searchTerm, setSearchTerm ]=useState('');
+    //const [ searchTerm, setSearchTerm ]=useState('');
+    const dispatch = useDispatch();
+    const searchTerm = useSelector(selectSearch);
 
     const handleChange = (event) =>{
         const userInput = event.target.value;
-        setSearchTerm(userInput);
+        dispatch(setSearchTerm(userInput));
     }
 
+    /* 
+
+    // extra feature. implement submit searchTerm to filter and display something else. 
+    // in reddit app. typing search brings up list of matching subreddits
+    // submitting search result tells reddit app to search for posts. communities, subreddits etc. 
     const handleSubmit = (event) =>{
         event.preventDefault();
         if(searchTerm){
             console.log(searchTerm);
+            dispatch(submitSearchTerm(searchTerm));
         }
         setSearchTerm('');
     }
+    */
 
     return (
         <header>
@@ -30,7 +42,7 @@ export function Search (){
                 <p className="appName">RedditMinimal</p>
             </Link>
             </div>
-            <form onSubmit={handleSubmit}>
+            <form >
                 <input className="searchBar" placeholder='Search' value={searchTerm} onChange={handleChange} />
             </form>
         </header>
