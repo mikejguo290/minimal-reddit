c2df252980539d306945ea62581920d321bb34c4
mikejguo290 <mikejguo290@gmail.com>
2021-10-25 23:20:28 +0100

    Homepage use list of subreddits from Subreddits slice in its useEffect.

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -4,15 +4,20 @@ import { useEffect } from 'react'
 import { useDispatch, useSelector  } from 'react-redux';
 import { fetchPostsBySubreddits , selectPosts } from '../../features/Posts/postsSlice';
 import { selectSearch } from '../../features/Search/searchSlice';
+import { selectSubreddits } from '../../features/Subreddits/subredditsSlice';
 
 export function Homepage(){
     const pageType = "home"
     const params = {subreddit:null, postId:null}
     const dispatch = useDispatch();
+    const subreddits = useSelector(selectSubreddits);
+    const subredditsList = subreddits.map(subreddit => subreddit.name);
+    // array (subredditsList) is an object. do not use it as a dependency. otherwise infinite useEffect loop.
+    // convert it to a json string first before passing it to the dependency array. still get loads of warnings!
 
     useEffect(()=>{
-        dispatch(fetchPostsBySubreddits(['webdev', 'reactjs']));
-    },[dispatch]);
+        dispatch(fetchPostsBySubreddits(subredditsList));
+    },[dispatch, JSON.stringify(subredditsList)]);
     
     const posts = useSelector(selectPosts);
     const searchTerm = useSelector(selectSearch);
