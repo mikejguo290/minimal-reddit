4f396c532c4a5b0a6ae22a8c05f4502bebe8ac6e
mikejguo290 <mikejguo290@gmail.com>
2021-10-27 16:43:38 +0100

    install and set up react and javascript time-ago modules to calculate relative time for comments and posts.

--- src/index.js
+++ src/index.js
@@ -2,6 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './app/App';
+import TimeAgo from 'javascript-time-ago';
+
+import en from 'javascript-time-ago/locale/en.json';
+TimeAgo.addDefaultLocale(en);
 
 ReactDOM.render(
   <React.StrictMode>
