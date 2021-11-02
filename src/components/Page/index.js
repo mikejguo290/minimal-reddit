b3a58d59b921ce9b94a64dec1a603547bac28c33
mikejguo290 <mikejguo290@gmail.com>
2021-11-02 23:40:54 +0000

    use store's error state to conditionally render an error message, and style it.

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -3,19 +3,22 @@ import { Subreddits } from '../../features/Subreddits';
 import { Posts } from '../../features/Posts';
 import { Banner } from '../Banner';
 import { useSelector } from 'react-redux';
-import { selectIsLoadingStatus } from '../../features/Posts/postsSlice';
+import { selectIsLoadingStatus, selectHasErrorStatus } from '../../features/Posts/postsSlice';
 
 export function Page(props){
     const { type, params, postIds } = props;
     const isSubredditPage = type === "subreddit";
     const subRedditName = params.subreddit;
     const isLoading = useSelector(selectIsLoadingStatus);
+    const error = useSelector(selectHasErrorStatus);
+    
     return (
         <>  
             { isSubredditPage && <Banner name={subRedditName}/>}
-            <main>       
+            <main>  
                 <div className="feed">
-                    <Posts  pageType={type} postIds={postIds} />
+                    { error && <div className="errorMessage">{error.message}</div> }
+                    { !error && <Posts  pageType={type} postIds={postIds} /> }
                 </div>
             </main>
             <aside>
