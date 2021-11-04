63261c73af8bbba5925a094965950cf2aa7ec413
mikejguo290 <mikejguo290@gmail.com>
2021-11-04 12:26:35 +0000

    try to optimise sort Posts process with a array.sort() with compare function.

--- src/features/Posts/index.js
+++ src/features/Posts/index.js
@@ -9,16 +9,32 @@ export function Posts (props){
     
     const { pageType, postIds } = props;
     const allPosts = useSelector(selectPosts);
-    // filter posts by the criteria that postIds array includes post.id
-    const posts = allPosts.filter(post => postIds.includes(post.id));
+    
+    // filter posts by the criteria that postIds array includes post.id 
+    const filteredPosts = allPosts.filter(post => postIds.includes(post.id));
+    const filteredPostsIsEmpty = filteredPosts.length===0;
 
-    const filteredPostsIsEmpty = posts.length===0;
+    let idsIndex={} // first create the dictionary to try and optimise the array.sort compare function.
+    for (const [index, postId] of postIds.entries()){ 
+        idsIndex[postId]=index;
+    }
+    const posts = [...filteredPosts]; //array.sort() sorts in-place which React won't allow, because sorting result of useSelector would change the store. 
+    //pseudocode: const sortedPosts = allPosts.sort((a,b)=> #index of a.id in orderList - #index of b.id in orderList );
+    posts.sort((a,b)=> idsIndex[a.id] - idsIndex[b.id]); 
+
+     /*
+    // alternative implementation of sort
+    const posts = postIds.map(postId=>{
+        return filteredPost.find(post => post.id===postId);
+    });
+    */
 
     const dispatch = useDispatch();
     const searchTerm = useSelector(selectSearch);
     const handleClick = () =>{
         dispatch(clearSearchTerm());
     }
+
     const emptySearchResult = (
         <div className="emptySearchResult">
             <h3>{`No posts matching "${searchTerm}"`}</h3>
