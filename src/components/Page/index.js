c0f13c679a0cf76f38c97210b9e1addf0b5ba4b2
mikejguo290 <mikejguo290@gmail.com>
2021-11-05 13:02:18 +0000

    modify Page to clear searchTerm only if navigating to new page AND searchTerm exists with useRef to compare previous navigation states.

--- src/components/Page/index.js
+++ src/components/Page/index.js
@@ -1,28 +1,53 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useRef } from 'react';
 import { Subreddits } from '../../features/Subreddits';
 import { Posts } from '../../features/Posts';
 import { Banner } from '../Banner';
 import { selectIsLoadingStatus, selectPostsError } from '../../features/Posts/postsSlice';
-import { clearSearchTerm } from '../../features/Search/searchSlice';
+import { clearSearchTerm, selectSearch } from '../../features/Search/searchSlice';
 
 export function Page(props){
     const { type, params, postIds } = props;
     const isSubredditPage = type === "subreddit";
-    const subRedditName = params.subreddit;
+    const subreddit = params.subreddit;
+    const postId = params.postId;
     const isLoading = useSelector(selectIsLoadingStatus);
     const error = useSelector(selectPostsError);
+    
+    const searchTerm = useSelector(selectSearch);
     const dispatch = useDispatch();
+    
+    const usePrevious = (value) =>{
+        // custom hook to get previous value of argument.
+        const ref = useRef(); // persists across renders
+        useEffect(()=>{
+            ref.current = value;
+        });
+        return ref.current; // returns on render, before useEffect. (in effect returns previous ref value)
+    }
+
+    const prevSubreddit = usePrevious(subreddit);
+    const prevPostId = usePrevious(postId);
+    const pageChange = prevSubreddit !== subreddit || prevPostId !== postId; // test to see if user navigated to new page.
 
     useEffect(()=>{
         // clear search term whenever user navigates to a different page/url.
-        dispatch(clearSearchTerm());
-    },[dispatch, params.subreddit, params.postId]);
+        // scenarios. 
+        // 1. same page typing into search => expect searchTerm to change
+        // 2. same page deleting search => expect searchTerm to diminish
+        // 3. navigating to a different page with empty searchTerm.
+        // 4. navigating to a different page with non-empty searchTerm. 
+
+        if(searchTerm && pageChange){
+            dispatch(clearSearchTerm());
+        }
+        
+    },[dispatch, searchTerm, pageChange]);
 
     return (
         <>  
-            { isSubredditPage && <Banner name={subRedditName}/>}
+            { isSubredditPage && <Banner name={subreddit}/>}
             <main>  
                 <div className="feed">
                     { error && <div className="errorMessage">{error.message}</div> }
