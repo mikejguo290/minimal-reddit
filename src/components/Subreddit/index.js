51d464d11c7d7ed2bd62aaadcaa8b312c8b73c2c
mikejguo290 <mikejguo290@gmail.com>
2021-11-07 01:50:55 +0000

    add and style subreddit icons to Subreddits.

--- src/components/Subreddit/index.js
+++ src/components/Subreddit/index.js
@@ -2,11 +2,15 @@ import redditImage from '../../images/reddit.jpeg';
 import React from 'react';
 
 export function Subreddit(props){
-    const { name } = props.data;
+    const { name, community_icon, icon_img } = props.data;
+
+    // use community_icon before icon_img and if both are absent, use the backup icon url.
+    const backupIconUrl = 'https://styles.redditmedia.com/t5_fl7c8/styles/communityIcon_mb09ajfn63241.png'
+    const subredditIconUrl = community_icon || icon_img || backupIconUrl;
     return (
         <div className="subreddit">
             <figure>
-                <img src={redditImage} alt='subreddit icon' />
+                <img className="subredditIcon" src={subredditIconUrl} alt='subreddit icon' />
             </figure>
             <h3>{name}</h3>
         </div>
--- src/features/Subreddits/subredditsSlice.js
+++ src/features/Subreddits/subredditsSlice.js
@@ -4,27 +4,41 @@ const sub1 = {
     name:'javascript', /* subreddit display_name */
     display_name_prefixed: "r/javascript",
     id:'t5_2qh30', /* match to subreddit name */
+    community_icon:'',
+    icon_img:'https://a.thumbs.redditmedia.com/zDOFJTXd6fmlD58VDGypiV94Leflz11woxmgbGY6p_4.png',
 }
 const sub2 = {
     name:'webdev',
     display_name_prefixed: "r/webdev",
     id:'t5_2qs0q', 
+    community_icon:'https://styles.redditmedia.com/t5_2qs0q/styles/communityIcon_5ey8lzmwmxp21.png',
+    icon_img:'',
 }
+
 const sub3 = {
     name:'reactjs',
     display_name_prefixed: "r/reactjs",
     id:'t5_2zldd', 
+    community_icon:'https://styles.redditmedia.com/t5_2zldd/styles/communityIcon_fbblpo38vy941.png',
+    icon_img:'',
 }
+
 const sub4={
     name:'learnprogramming',
     display_name_prefixed: "r/learnprogramming",
     id:'t5_2r7yd',
+    community_icon:'',
+    icon_img:'',
 }
+
 const sub5={
     name:'ProgrammerHumor',
     display_name_prefixed: "r/ProgrammerHumor",
     id:'t5_2tex6',
+    community_icon:'https://styles.redditmedia.com/t5_2tex6/styles/communityIcon_u89jf60zv7p41.png',
+    icon_img:'https://b.thumbs.redditmedia.com/Qj8PVGSQ_B_3JjNCeE-bxz-RVokmZQ23i8cNGRw7Nhc.png',
 }
+
 const subreddits = [sub1, sub2, sub3, sub4, sub5];
 
 const options = {
