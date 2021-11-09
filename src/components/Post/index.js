d7c232b75b9afe3ef32862dd1148ade5cb3e292a
mikejguo290 <mikejguo290@gmail.com>
2021-11-09 18:59:41 +0000

    convert Post to use helpers that handle null inputs. Links and other components render conditionally.

--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -24,6 +24,9 @@ export function Post(props){
         num_comments,
     } = props.data;
 
+    const commentsError = useSelector(selectCommentsError);
+    const loading = useSelector(selectIsLoadingStatus);
+    
     /* 
     let page know if a request came in to view detailed Page view. this should be an attribute of a specific Post object.
     to allow for viewing comments in homepage or subreddit, without clicking through to PostDetail page.
@@ -32,8 +35,8 @@ export function Post(props){
     const selfTextHtmlExists = selftext_html !=null;
     const urlIsImage = checkUrlIsImage(url);
     const urlContainsPostId = checkUrlContainsPostId(url, id);
-    const commentsError = useSelector(selectCommentsError);
-    const loading = useSelector(selectIsLoadingStatus);
+    // render link only if it's not image or postId isn't in the link (which is just a duplicate of page url
+    const showResourceLink = urlIsImage === false && urlContainsPostId === false;
    
     // JSX elements to be rendered
     const bodyImage = urlIsImage && <figure><img src={url} alt={`${subreddit_name_prefixed} - ${title}`} /></figure>;
@@ -46,7 +49,7 @@ export function Post(props){
         // a lot of data validation goes even AFTER the loading is done. Can't skip these even when using loading state. 
         bodyText = selfTextHtmlExists && <div className="selftextHtml" dangerouslySetInnerHTML={createMarkup(selftext_html)} />
         // render link only if it's not image or postId isn't in the link (which is just a duplicate of page url)
-        resourceLink = !urlIsImage && !urlContainsPostId && <a className="postedResource" href={url}>{url}</a>
+        resourceLink = showResourceLink && <a className="postedResource" href={url}>{url}</a>
         // comments section is either an error message if error loading comments or the Comments component.
         commentsSection = commentsError
             ?<div className="errorMessage">{`${commentsError.message} comments`}</div>
@@ -60,10 +63,10 @@ export function Post(props){
             </div>
             <div className="postContext">
                 <div className="postContent">
-                    <Link to={`/r/${subreddit}`} >
+                    <Link to={`/r/${subreddit || ''}`} >
                         <p className='postSubreddit'>{ loading? <Skeleton width={"4em"}/> : subreddit_name_prefixed }</p>
                     </Link>
-                    <Link to={permalink} className="postLink">
+                    <Link to={permalink || ''} className="postLink">
                         <h3>{ loading? <Skeleton height={"1.17em"} width={"50%"}/> : title }</h3>
                     </Link>
                     { loading? <Skeleton/> : bodyText } 
@@ -77,7 +80,7 @@ export function Post(props){
                     </div>
                     {loading? <Skeleton width={"2em"} /> :<ReactTimeAgo date={created_utc*1000} locale="en-GB" timeStyle="mini-minute-now"/>}
                     {loading && <Skeleton width={'4em'} />}
-                    <Link to={permalink} className="postLink" style={{display:loading? 'none': 'undefined'}}>
+                    <Link to={permalink || ''} className="postLink" style={{display:loading? 'none': 'undefined'}}>
                         <div className="commentNumberContainer">
                             <span className="material-icons-outlined">
                                 comment
