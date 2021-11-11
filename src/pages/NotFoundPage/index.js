2cfd450b60eda84f383aab81b8c8a604d5e76a1d
mikejguo290 <mikejguo290@gmail.com>
2021-11-11 12:12:09 +0000

    modify NotFoundPage to access state.error in location and output bespoke error messages.

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,21 +1,31 @@
 import React from 'react';
 import { PageTemplate } from '../../components/PageTemplate';
 import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router';
 
 export const NotFoundPage = () => {
     /* 
-    page to handle urls that doesn't map to anything set out in Router on App.js 
-    uses PageTempate directly, rather than use Page.
+    this page handle urls that doesn't map to anything set out in Router on App.js
+    react router history also redirect users to this page when they encounter posts loading errors. 
+    this page uses PageTempate directly, rather than use Page.
     */
-    const errorMessage = 'Something has gone wrong'; // error message with default value.
+    const location = useLocation();
     
+    // test location for state passed in by the redirect used in history.push()
+    let errorMessage;
+    if(location.state !== undefined && location.state.error){ // if location has state && location.state has error
+        errorMessage = location.state.error.message;
+    }else{
+        errorMessage = 'Something has gone wrong'; // error message default value.
+    }
+  
     return (
         <>
             <PageTemplate>
                     <div className="notFound">  
                         <h3>{errorMessage}</h3>
                         <Link to="/">
-                            <button>Start again</button>
+                            <button>Back to homepage</button>
                         </Link>
                     </div>
             </PageTemplate>
