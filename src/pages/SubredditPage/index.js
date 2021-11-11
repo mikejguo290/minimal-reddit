e48215f3aeb6c6f1a4cc4bc8177b3c725a905266
mikejguo290 <mikejguo290@gmail.com>
2021-11-11 13:34:35 +0000

    redirect user to error page if there is posts loading error.

--- src/pages/SubredditPage/index.js
+++ src/pages/SubredditPage/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Page } from '../../components/Page';
 import { useParams } from 'react-router-dom';
-import { useEffect, useRef } from 'react';
+import { useEffect } from 'react';
 import { selectSearch } from '../../features/Search/searchSlice';
 import { fetchPostsBySubreddits , selectPosts , selectPostsError} from '../../features/Posts/postsSlice';
 import { selectComments } from '../../features/Comments/commentsSlice';
 import { useDispatch , useSelector  } from 'react-redux';
+import { useHistory } from 'react-router';
 
 export function SubredditPage(){
     /* 
@@ -21,17 +22,29 @@ export function SubredditPage(){
     along with pageType, and subreddit name extracted from the url params. 
     */
     const dispatch = useDispatch();
+    const history = useHistory();
+    // page type and params
     const pageType = "subreddit"
     const { subreddit } = useParams();
     const params = {subreddit:subreddit, postId:null}
-    
+
+    // load posts error
+    const error = useSelector(selectPostsError);
+    // posts and derivatives
     const posts = useSelector(selectPosts); // once posts is updated, this page would rerender and pass filteredPost as props to page. 
-    const searchTerm = useSelector(selectSearch);
     const subredditPosts = posts.filter(post => post.subreddit === subreddit); 
     const subPostsInStore = subredditPosts.length;
     // important to decouple subredditPosts from filteredPosts.  even though the second is derived from the first.
     // subredditsPosts.length can trigger an api call to fetch more posts
     // whilst filteredPosts is used to create list of postIds passed to <Page />.
+    
+    // comments 
+    const comments = useSelector(selectComments); // { postIdx: [comments of post x]}
+    const commentIds = Object.keys(comments) // returns list of postIds 
+    const commentMatchPost = subredditPosts.find(post => post.id === commentIds[0] )? true: false;
+    // searchTerm
+    const searchTerm = useSelector(selectSearch);
+
     const filteredPosts = subredditPosts.filter(post => {
         if(searchTerm){
             return post.title.toLowerCase().includes(searchTerm.toLowerCase())
@@ -42,32 +55,32 @@ export function SubredditPage(){
     
     const postIds = filteredPosts.map(post => post.id);
 
-    // fetch posts error
-    const error = useSelector(selectPostsError);
-    // comments 
-    const comments = useSelector(selectComments); // { postIdx: [comments of post x]}
-    const commentIds = Object.keys(comments) // returns list of postIds 
-    const commentMatchPost = subredditPosts.find(post => post.id === commentIds[0] )? true: false;
-
-    const prevSubreddit = useRef();
     useEffect(()=>{
+        // dispatch C.A.T to make API call if - 
         // if subreddit posts equal to 0, (assuming this reddit's posts are not loaded in inital api call)
         // if subredditPosts equal to 1 and comment matches post id (assuming this is loading data for a specific post by landing on the url for that post first thing)
         // call api to get that subreddit's posts. 
         // unless there is a fetch post error, in which case do not execute callback in effect.  
         if( !error && subPostsInStore === 0 ){
+            console.log('making a call because there are zero store posts.');
             dispatch(fetchPostsBySubreddits([subreddit]));
         }
         if( !error && subPostsInStore === 1 && commentMatchPost ){
             dispatch(fetchPostsBySubreddits([subreddit]));
+        } 
+
+        if (error){
+            history.push({
+                pathname:"/error",
+                state:{ error: error}
+            }) // imperatively redirect to NotFoundPage. 
         }
-        // if error AND subreddit param in the url has changed. dispatch C.A.T to fetch subreddit posts. 
-        // this allows the user to click around after first trying to access an invalid api endpoint with their own url.
-        if ( error && subreddit !== prevSubreddit.current ){
-            dispatch(fetchPostsBySubreddits([subreddit]));
-        }
-        prevSubreddit.current = subreddit;
-    },[error, subreddit, subPostsInStore, commentMatchPost, dispatch]);
+
+        // *** return cleanup on component dismount to reset posts loading error state in store.
+        // flickering so useLayoutEffect? 
+        // clean up unnecessary bits. 
+
+    },[error, subreddit, subPostsInStore, commentMatchPost, history, dispatch]);
    
     return (
         <Page type={pageType} params={params} postIds={postIds} />
