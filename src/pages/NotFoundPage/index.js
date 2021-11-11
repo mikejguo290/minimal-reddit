960e938b47eacbcdc55d6c12a64f5c8ac3fb98a6
mikejguo290 <mikejguo290@gmail.com>
2021-11-11 14:24:53 +0000

    NotFoundPage dispatch action resetError to clear posts loading error in store after rendering.

--- src/pages/NotFoundPage/index.js
+++ src/pages/NotFoundPage/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { PageTemplate } from '../../components/PageTemplate';
 import { Link } from 'react-router-dom';
 import { useLocation } from 'react-router';
-
+import { useDispatch } from 'react-redux'
+import { resetError } from '../../features/Posts/postsSlice';
 export const NotFoundPage = () => {
     /* 
     this page handle urls that doesn't map to anything set out in Router on App.js
     react router history also redirect users to this page when they encounter posts loading errors. 
-    this page uses PageTempate directly, rather than use Page.
+    it resets loading error states in store. 
+    (this page uses PageTempate directly, rather than use Page.)
     */
+    const dispatch = useDispatch();
     const location = useLocation();
     
     // test location for state passed in by the redirect used in history.push()
@@ -18,7 +21,11 @@ export const NotFoundPage = () => {
     }else{
         errorMessage = 'Something has gone wrong'; // error message default value.
     }
-  
+    // clear all loading errors
+    useEffect(()=>{
+        dispatch(resetError());
+    },[dispatch])
+
     return (
         <>
             <PageTemplate>
