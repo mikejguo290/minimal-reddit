f023dd6c5b9d91a734d05196c56b5a62482d02be
mikejguo290 <mikejguo290@gmail.com>
2021-11-22 13:26:31 +0000

    replace useLayoutEffect hook with useDeepCompareEffect hook to be able to use the array subredditsList in dependency array.

--- src/pages/Homepage/index.js
+++ src/pages/Homepage/index.js
@@ -6,6 +6,7 @@ import { fetchPostsBySubreddits , selectPosts, clearPosts } from '../../features
 import { selectSearch } from '../../features/Search/searchSlice';
 import { selectSubreddits } from '../../features/Subreddits/subredditsSlice';
 import { mixPosts } from '../../utils/helper'
+import { useDeepCompareEffect } from 'react-use';
 
 export function Homepage(){
     const pageType = "home"
@@ -22,25 +23,25 @@ export function Homepage(){
                                             : post);
     const postIds = mixPosts(filteredPosts);
 
-    // effect runs on every render of Homepage. 
-    /* cannot use subredditsList as a dependency array because it is a list and each rerender would 
-    get a different list (as it appears to javascript,) causing infinite rerenders
-    JSON.stringify(s) converts array into string of ["javascript","webdev","reactjs","learnprogramming","ProgrammerHumor"]
-    which doesn't change until subredditList's contents actually change. 
-    */
 
+    // the effect (api call) introduces DOM mutations which change the appearance of the DOM between component first rendering (with old data) and when API call finishes (with new data).
+    // useLayoutEffect differs from useEffect in that it runs before React paints.
+    // useEffect runs only after the component initially renders with old data, which introduces a visual glitch before loading skeletons displays.
     useLayoutEffect(()=>{
         // clear posts in store before loading. this is to attain a completely new set of posts on each home page load.
         // fetchPostsBySubreddits doesn't fire if there is at least one post per subreddit. 
         dispatch(clearPosts());
     },[dispatch]);
-
-    useLayoutEffect(()=>{
-        // the effect (api call) introduces DOM mutations which change the appearance of the DOM between component first rendering (with old data) and when API call finishes (with new data).
-        // useLayoutEffect differs from useEffect in that it runs before React paints.
-        // useEffect runs only after the component initially renders with old data, which introduces a visual glitch before loading skeletons displays.
+ 
+    /* cannot use subredditsList as a dependency array with useEffect because it is a list and each rerender would 
+    get a different list (as it appears to javascript,) causing infinite rerenders
+    JSON.stringify(s) converts array into string of ["javascript","webdev","reactjs","learnprogramming","ProgrammerHumor"]
+    which doesn't change until subredditList's contents actually change. this breaks ESLint rules however.
+    to stop breaking ESLint rules, use useDeepCompareEffect to make a deep comparison of the subredditsList array.
+    */
+    useDeepCompareEffect(()=>{
         dispatch(fetchPostsBySubreddits(subredditsList));
-    },[dispatch, JSON.stringify(subredditsList)]);
+    },[dispatch, subredditsList]);
 
     return (
         <Page type={pageType} params={params} postIds={postIds} />
